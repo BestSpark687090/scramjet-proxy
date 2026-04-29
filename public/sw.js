4d365f91b76e7512cde366dba027210acd603f94
BestSpark687090 <73793381+BestSpark687090@users.noreply.github.com>
2026-04-28 22:49:56 -0500

    bundled

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts("/scram/scramjet.all.js");
+importScripts("/scram/scramjet_bundled.js");
 importScripts("/config.js");
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
